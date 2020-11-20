cca23c38ba4cbe5f2c8d3112ebb9ebef993f0430
Jibreel Yameen <jibreelyameen@Jibreels-MacBook-Pro.local>
2020-11-19 20:33:25 -0500

    commit11

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// Here's a test channel number, check the console!
+console.log(getRandomColorChannel())
+
+let theBody = document.querySelector('body')
+let theMsg = document.querySelector('.colour ')
+let theBtn = document.querySelector('.randomCol')
+
+
+    let setNewColour = function() {
+        
+        let redCol = getRandomColorChannel()
+        let greenCol = getRandomColorChannel()
+        let blueCol = getRandomColorChannel()
+        
+        theBody.setAttribute(`style`, `background-color: rgb(${redCol},${greenCol},${blueCol})`)
+        
+        theMsg.textContent = `rgb(${redCol},${greenCol},${blueCol})`
+         }
+
+theBtn.addEventListener('mousemove', setNewColour)        
+
+
+
+
+// 1) Built a colour string using `getRandomColorChannel()` for each of the three colour channels (R, G, B)
+
+// 2) Update user interface (`document`), setting the new colour to the body's background
+
+// 3) Use the new RGB value to update the label "rgb(###, ####, ###)" (replacing ###s with the Number values)
+
+// 4) Wrap the random colour procedure in a function named `setNewColour()`, call it from the `console` to test
+
+//theBody.style.setProperty(`background-color`, `rgb(${rCol},${gCol},${bCol})`)//
